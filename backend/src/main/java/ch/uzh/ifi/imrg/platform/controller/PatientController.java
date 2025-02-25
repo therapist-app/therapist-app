97b4bdfb7bceee1d47d3685516ffc199ecf6b05d
Daniel Maksimovic <daniel.maksimovic01@gmail.com>
2025-02-26 00:43:33 +0100

    16: Create Patient Endpoint

---
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/controller/PatientController.java
@@ -0,0 +1,50 @@
+package ch.uzh.ifi.imrg.platform.controller;
+
+import ch.uzh.ifi.imrg.platform.rest.dto.input.PatientInputDTO;
+import ch.uzh.ifi.imrg.platform.rest.dto.output.PatientOutputDTO;
+import ch.uzh.ifi.imrg.platform.service.PatientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping("/patients")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PatientOutputDTO createPatient(@RequestBody PatientInputDTO patientInputDTO) {
+        return patientService.createPatient(patientInputDTO);
+    }
+
+    @GetMapping("/patients")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PatientOutputDTO> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping("/therapists/{therapistId}/patients")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PatientOutputDTO> getPatientsForTherapist(@PathVariable String therapistId) {
+        return patientService.getPatientsForTherapist(therapistId);
+    }
+
+    @GetMapping("/patients/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PatientOutputDTO getPatientById(@PathVariable Long id) {
+        return patientService.getPatientById(id);
+    }
+
+    @PostMapping("/therapists/{therapistId}/patients")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PatientOutputDTO createPatientForTherapist(@PathVariable String therapistId,
+                                                      @RequestBody PatientInputDTO inputDTO) {
+        return patientService.createPatientForTherapist(therapistId, inputDTO);
+    }
+}
--- backend/src/main/java/ch/uzh/ifi/imrg/platform/entity/Therapist.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/entity/Therapist.java
@@ -1,12 +1,10 @@
 package ch.uzh.ifi.imrg.platform.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "therapists")
@@ -22,10 +20,15 @@ public class Therapist implements Serializable {
     @Column(nullable = false)
     private String password;
 
+    @Column(unique = true)
+    private String workspaceId = UUID.randomUUID().toString();
+
+    @OneToMany(mappedBy = "therapist", fetch = FetchType.LAZY)
+    private List<Patient> patients = new ArrayList<>();
+
     public void setId(String id) {
         this.id = id;
     }
-
     public String getId() {
         return id;
     }
@@ -33,7 +36,6 @@ public class Therapist implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getEmail() {
         return email;
     }
@@ -41,8 +43,21 @@ public class Therapist implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getPassword() {
         return password;
+    }
+
+    public String getWorkspaceId() {
+        return workspaceId;
+    }
+    public void setWorkspaceId(String workspaceId) {
+        this.workspaceId = workspaceId;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
     }
 }
