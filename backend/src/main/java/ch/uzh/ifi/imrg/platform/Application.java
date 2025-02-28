4c2543ff0ef8e04fba3d0cb1b355f220d6474a51
Jonas Blum <jonas.blum2@uzh.ch>
2025-02-28 10:55:53 +0100

    fixing things

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/Application.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/Application.java
@@ -12,11 +12,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
-
-
-
 @RestController
 @SpringBootApplication
 public class Application {
