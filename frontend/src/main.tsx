559543ef9c9d8857288d95a8d2facc96d4a82040
Jonas Blum <jonas.blum2@uzh.ch>
2025-02-28 10:09:07 +0100

    test commit 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,11 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+
+
+
+
+
  createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
