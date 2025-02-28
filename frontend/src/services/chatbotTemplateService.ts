6247e62bf738728f2b9855ac2ea22db6db7c296b
Jonas Blum <jonas.blum2@uzh.ch>
2025-02-28 11:01:00 +0100

    t

--- frontend/src/services/chatbotTemplateService.ts
+++ frontend/src/services/chatbotTemplateService.ts
@@ -2,7 +2,7 @@ import { CreateChatbotTemplateDTO } from "../dto/input/CreateChatbotTemplateDTO"
 import { ChatbotTemplateOutputDTO } from "../dto/output/ChatbotTemplateOutputDTO";
 import api from "../utils/api";
 
-export async function getAllChatbotTemplatesForTherapist(therapistId: string): Promise<any[]> {
+export async function getAllChatbotTemplatesForTherapist(therapistId: string): Promise<ChatbotTemplateOutputDTO[]> {
   const response = await api.get(`/api/therapists/${therapistId}/chatbot-templates`);
   return response.data;
 }
