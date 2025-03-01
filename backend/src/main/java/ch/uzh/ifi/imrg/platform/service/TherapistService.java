c47a22e62d7da8d5d7d0cb178fda5c2dd7487497
Jonas Blum <jonas.blum2@uzh.ch>
2025-03-01 17:38:42 +0100

    .

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/service/TherapistService.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/service/TherapistService.java
@@ -3,8 +3,6 @@ package ch.uzh.ifi.imrg.platform.service;
 import ch.uzh.ifi.imrg.platform.entity.Therapist;
 import ch.uzh.ifi.imrg.platform.repository.TherapistRepository;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
