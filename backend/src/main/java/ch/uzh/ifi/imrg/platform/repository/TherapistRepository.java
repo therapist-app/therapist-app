eefed50896be92a06e0de320f08521f9d7c25a36
Jonas Blum <jonas.blum2@uzh.ch>
2025-03-01 17:33:22 +0100

    adding jwt util

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/repository/TherapistRepository.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/repository/TherapistRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface TherapistRepository extends JpaRepository<Therapist, String> {
 
   boolean existsByEmail(String email);
+
+  Therapist getTherapistByEmail(String email);
 }
--- backend/src/main/java/ch/uzh/ifi/imrg/platform/service/TherapistService.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/service/TherapistService.java
@@ -2,6 +2,8 @@ package ch.uzh.ifi.imrg.platform.service;
 
 import ch.uzh.ifi.imrg.platform.entity.Therapist;
 import ch.uzh.ifi.imrg.platform.repository.TherapistRepository;
+import ch.uzh.ifi.imrg.platform.rest.dto.input.LoginTherapistDTO;
+
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +23,6 @@ public class TherapistService {
   public TherapistService(
       @Qualifier("therapistRepository") TherapistRepository therapistRepository) {
     this.therapistRepository = therapistRepository;
-    initializeSomeTherapists();
   }
 
   public Therapist createTherapist(Therapist therapist) {
@@ -46,36 +47,18 @@ public class TherapistService {
     return this.therapistRepository.save(therapist);
   }
 
+  public void loginTherapist(LoginTherapistDTO loginTherapistDTO) {
+    Therapist foundTherapist = therapistRepository.getTherapistByEmail(loginTherapistDTO.getEmail());
+    if (foundTherapist == null) {
+      throw new Error("No therapist with email: " + loginTherapistDTO.getEmail() + " exists");
+    }
+    if (foundTherapist.getPassword() != loginTherapistDTO.getPassword()) {
+      throw new Error("The password you entered is wrong!");
+    }
+  }
+
   public List<Therapist> getAllTherapists() {
     return this.therapistRepository.findAll();
   }
 
-  private void initializeSomeTherapists() {
-    Therapist therapist1 = new Therapist();
-    therapist1.setId("bf2a19d5-3037-4b65-86dd-cec232ec66b1");
-    therapist1.setEmail("admin@admin.com");
-    therapist1.setPassword("admin");
-
-    Therapist therapist2 = new Therapist();
-    therapist2.setId("6af0e118-c344-4444-8681-2d2e56cf6302");
-    therapist2.setEmail("test@test.com");
-    therapist2.setPassword("test");
-
-    Therapist therapist3 = new Therapist();
-    therapist3.setId("f8fda83a-995f-4bf5-8604-89e10d287738");
-    therapist3.setEmail("therapist@therapist.com");
-    therapist3.setPassword("therapist");
-
-    if (!this.therapistRepository.existsById(therapist1.getId())) {
-      this.therapistRepository.save(therapist1);
-    }
-
-    if (!this.therapistRepository.existsById(therapist2.getId())) {
-      this.therapistRepository.save(therapist2);
-    }
-
-    if (!this.therapistRepository.existsById(therapist3.getId())) {
-      this.therapistRepository.save(therapist3);
-    }
-  }
 }
