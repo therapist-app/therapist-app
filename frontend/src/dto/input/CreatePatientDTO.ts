260fb96cba0e8c5257aeb07df398c8e9e279616a
AlexBac <alex.bacmann9@gmail.com>
2025-03-02 15:58:40 +0100

    fix

--- frontend/src/dto/input/CreatePatientDTO.ts
+++ frontend/src/dto/input/CreatePatientDTO.ts
@@ -1,9 +1,9 @@
 export interface CreatePatientDTO {
-  name: string;
-  gender: string;
-  age: number;
-  phoneNumber: string;
-  email: string;
-  address: string;
-  description: string;
+  name: string
+  gender: string
+  age: number
+  phoneNumber: string
+  email: string
+  address: string
+  description: string
 }
