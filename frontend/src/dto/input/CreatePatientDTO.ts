06092cbfd83ec52914829df110bd79acfa697cdc
AlexBac <alex.bacmann9@gmail.com>
2025-03-02 14:29:24 +0100

    Register a patient from the Dashboard

--- frontend/src/dto/input/CreatePatientDTO.ts
+++ frontend/src/dto/input/CreatePatientDTO.ts
@@ -1,3 +1,9 @@
 export interface CreatePatientDTO {
-  name: string
+  name: string;
+  gender: string;
+  age: number;
+  phoneNumber: string;
+  email: string;
+  address: string;
+  description: string;
 }
