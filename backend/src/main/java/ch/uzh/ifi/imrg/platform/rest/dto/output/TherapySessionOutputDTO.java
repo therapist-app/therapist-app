76ac715d9e77991bcebe4a4ce1f7f141e10b27f7
Jonas Blum <jonas.blum2@uzh.ch>
2025-03-19 16:58:00 +0100

    Adding therapySession stuff in the backend

---
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/rest/dto/output/TherapySessionOutputDTO.java
@@ -0,0 +1,38 @@
+
+package ch.uzh.ifi.imrg.platform.rest.dto.output;
+
+import java.time.LocalDateTime;
+
+public class TherapySessionOutputDTO {
+
+    private String id;
+
+    private LocalDateTime sessionStart;
+
+    private LocalDateTime sessionEnd;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getSessionStart() {
+        return sessionStart;
+    }
+
+    public void setSessionStart(LocalDateTime sessionStart) {
+        this.sessionStart = sessionStart;
+    }
+
+    public LocalDateTime getSessionEnd() {
+        return sessionEnd;
+    }
+
+    public void setSessionEnd(LocalDateTime sessionEnd) {
+        this.sessionEnd = sessionEnd;
+    }
+
+}
