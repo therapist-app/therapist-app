7861e11c572da5c67f99a4a84822a55deba25680
Jonas Blum <jonas.blum2@uzh.ch>
2025-03-19 13:54:30 +0100

    85: Adjusting layout

--- frontend/src/pages/patients/PatientDetail.tsx
+++ frontend/src/pages/patients/PatientDetail.tsx
@@ -1,17 +1,18 @@
 import { Typography } from '@mui/material'
 import { useParams } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import Layout from '../../generalComponents/Layout'
 
 const PatientDetail = () => {
   const { patientId } = useParams()
   const { t } = useTranslation()
 
   return (
-    <div>
+    <Layout>
       <Typography variant='h3'>
         {t('patient_detail.message')}: "{patientId}"
       </Typography>
-    </div>
+    </Layout>
   )
 }
 
