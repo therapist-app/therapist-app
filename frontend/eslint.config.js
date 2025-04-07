41246352ed8964b15157505f72f1f50feebb1592
Jonas Blum <jonas.blum2@uzh.ch>
2025-04-07 22:13:33 +0200

    .

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'src/api/**'] },
+  { ignores: ['dist', 'src/api/**', 'src/@types/resources.d.ts'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
