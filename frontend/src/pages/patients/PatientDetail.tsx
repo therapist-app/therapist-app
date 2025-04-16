1e3ccac50180796268754f741f5274cbca538174
daniel-maksimovic <daniel.maksimovic@uzh.ch>
2025-04-16 13:36:58 +0200

    Create Chatbot in Patient Details

--- frontend/src/pages/patients/PatientDetail.tsx
+++ frontend/src/pages/patients/PatientDetail.tsx
@@ -9,6 +9,12 @@ import {
   TableHead,
   TableRow,
   Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
 } from '@mui/material'
 import { useNavigate, useParams } from 'react-router-dom'
 import Layout from '../../generalComponents/Layout'
@@ -34,6 +40,7 @@ const PatientDetail = () => {
   const { patientId } = useParams()
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
+
   const allPatientDocuments = useSelector(
     (state: RootState) => state.patientDocument.allPatientDocumentsOfPatient
   )
@@ -43,6 +50,9 @@ const PatientDetail = () => {
 
   const [refreshPatientDocumentsCounter, setRefreshPatientDocumentsCounter] = useState(0)
 
+  const [openChatbotDialog, setOpenChatbotDialog] = useState(false)
+  const [chatbotName, setChatbotName] = useState('')
+
   useEffect(() => {
     dispatch(getAllPatientDocumentsOfPatient(patientId ?? ''))
   }, [dispatch, patientId, refreshPatientDocumentsCounter])
@@ -50,7 +60,7 @@ const PatientDetail = () => {
   const handleFileUpload = async (file: File) => {
     await dispatch(
       createDocumentForPatient({
-        file: file,
+        file,
         patientId: patientId ?? '',
       })
     )
@@ -67,10 +77,8 @@ const PatientDetail = () => {
       const response = await patientDocumentApi.downloadFile(fileId, {
         responseType: 'blob',
       })
-
       const file = response.data
       const url = window.URL.createObjectURL(file)
-
       const link = document.createElement('a')
       link.href = url
       link.setAttribute('download', fileName)
@@ -100,14 +108,71 @@ const PatientDetail = () => {
     )
   }
 
+  const handleOpenChatbotDialog = () => {
+    setOpenChatbotDialog(true)
+  }
+
+  const handleCloseChatbotDialog = () => {
+    setOpenChatbotDialog(false)
+    setChatbotName('')
+  }
+
+  const handleCreateNewChatbot = () => {
+    console.log('Chatbot created with name:', chatbotName)
+
+    navigate(
+      getPathFromPage(PAGES.CHATBOT_TEMPLATES_DETAILS_PAGE, {
+        chatbotTemplateId: 'newBot',
+      })
+    )
+
+    handleCloseChatbotDialog()
+  }
+
   return (
     <Layout>
       <Typography variant='h4'>Patient details of "{patientId}"</Typography>
 
+      <Button
+        variant='contained'
+        onClick={handleOpenChatbotDialog}
+        sx={{ marginTop: '20px', marginBottom: '20px' }}
+      >
+        Create new Chatbot
+      </Button>
+
+      <Dialog open={openChatbotDialog} onClose={handleCloseChatbotDialog}>
+        <DialogTitle>Create a new Chatbot</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter a name for your new chatbot:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin='dense'
+            label='Chatbot Name'
+            type='text'
+            fullWidth
+            variant='outlined'
+            value={chatbotName}
+            onChange={(e) => setChatbotName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseChatbotDialog}>Cancel</Button>
+          <Button
+            onClick={handleCreateNewChatbot}
+            variant='contained'
+            disabled={!chatbotName.trim()}
+          >
+            Create Bot
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <Typography sx={{ marginTop: '50px' }} variant='h4'>
         All Files
       </Typography>
-
       <TableContainer sx={{ marginTop: '10px' }} component={Paper}>
         <Table aria-label='simple table'>
           <TableHead>
