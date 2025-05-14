eb94cfc06df307a9918be758cf60e036840e2bf8
Jonas Blum <jonas.blum2@uzh.ch>
2025-05-14 13:38:07 +0200

    WIP

--- frontend/src/pages/exercises/ExerciseDetail.tsx
+++ frontend/src/pages/exercises/ExerciseDetail.tsx
@@ -1,11 +1,54 @@
-import { ReactElement } from 'react'
+import { Typography } from '@mui/material'
+import { ReactElement, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
 
+import { ExerciseFileOutputDTO, ExerciseTextOutputDTO } from '../../api'
 import Layout from '../../generalComponents/Layout'
+import { getExerciseById } from '../../store/exerciseSlice'
+import { RootState } from '../../store/store'
+import { useAppDispatch } from '../../utils/hooks'
+
+interface SortedExerciseDetails {
+  type: 'file' | 'text'
+  exerciseFile: ExerciseFileOutputDTO
+  exerciseText: ExerciseTextOutputDTO
+  orderNumber: number
+}
 
 const ExerciseDetail = (): ReactElement => {
+  const navigate = useNavigate()
+  const { patientId, therapySessionId, exerciseId } = useParams()
+  const dispatch = useAppDispatch()
+  const [sortedExerciseDetails, setSortedExerciseDetails] = useState<SortedExerciseDetails[]>([])
+
+  const selectedExercise = useSelector((state: RootState) => state.exercise.selectedExercise)
+
+  useEffect(() => {
+    dispatch(getExerciseById(exerciseId ?? ''))
+  }, [dispatch, exerciseId])
+
+  useEffect(() => {
+    if (
+      selectedExercise?.exerciseFilesOutputDTO === undefined ||
+      selectedExercise.exerciseTextsOutputDTO === undefined
+    ) {
+      return
+    }
+    const totalNumberOfDetails =
+      selectedExercise.exerciseFilesOutputDTO.length +
+      selectedExercise.exerciseTextsOutputDTO.length
+    for (let i = 0; i < totalNumberOfDetails; i++) {
+      //
+    }
+  }, [selectedExercise])
+
   return (
     <Layout>
-      <div>Exercise Detail</div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <Typography variant='h5'>Title: {selectedExercise?.title}</Typography>
+        <Typography>Exercise Type: {selectedExercise?.exerciseType}</Typography>
+      </div>
     </Layout>
   )
 }
