2a424dc83dabde1f3a65690aaac2c7d1f9c395ec
Jonas Blum <jonas.blum2@uzh.ch>
2025-05-19 12:04:22 +0200

    .

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/EnvironmentVariables.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/EnvironmentVariables.java
@@ -7,14 +7,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class EnvironmentVariables {
 
-  private static String jwtSecretKey;
+  static String JWT_SECRET_KEY;
+  static String LOCAL_LLM_URL;
+  static String LOCAL_LLM_API_KEY;
+  static String CHAT_GPT_API_KEY;
 
   @Autowired
-  public EnvironmentVariables(@Value("${JWT_SECRET_KEY}") String jwtSecretKey) {
-    EnvironmentVariables.jwtSecretKey = jwtSecretKey;
-  }
-
-  public static String getJwtSecretKey() {
-    return jwtSecretKey;
+  public EnvironmentVariables(
+      @Value("${JWT_SECRET_KEY}") String JWT_SECRET_KEY,
+      @Value("${LOCAL_LLM_URL}") String LOCAL_LLM_URL,
+      @Value("${LOCAL_LLM_API_KEY}") String LOCAL_LLM_API_KEY,
+      @Value("${CHAT_GPT_API_KEY}") String CHAT_GPT_API_KEY) {
+    EnvironmentVariables.JWT_SECRET_KEY = JWT_SECRET_KEY;
+    EnvironmentVariables.LOCAL_LLM_URL = LOCAL_LLM_URL;
+    EnvironmentVariables.LOCAL_LLM_API_KEY = LOCAL_LLM_API_KEY;
+    EnvironmentVariables.CHAT_GPT_API_KEY = CHAT_GPT_API_KEY;
   }
 }
