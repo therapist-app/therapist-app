83e557292ee4f43563f768d0b6cb1852e4244de6
Jonas Blum <jonas.blum2@uzh.ch>
2025-05-21 21:09:57 +0200

    Hardcoding api path (#179)

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/PatientAppAPIs.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/PatientAppAPIs.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.imrg.platform.utils;
 
 import ch.uzh.ifi.imrg.generated.api.PatientControllerPatientAPI;
+import ch.uzh.ifi.imrg.generated.invoker.ApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,10 @@ public class PatientAppAPIs {
   @Autowired
   public PatientAppAPIs() {
     String PATIENT_APP_URL = EnvironmentVariables.PATIENT_APP_URL;
+    System.out.println("PATIENT_APP_URL: " + PATIENT_APP_URL);
     patientControllerPatientAPI = new PatientControllerPatientAPI();
-    patientControllerPatientAPI.getApiClient().setBasePath(PATIENT_APP_URL);
+    ApiClient apiClient = patientControllerPatientAPI.getApiClient();
+    apiClient.setBasePath("https://backend-patient-app-main.jonas-blum.ch");
+    patientControllerPatientAPI.setApiClient(apiClient);
   }
 }
