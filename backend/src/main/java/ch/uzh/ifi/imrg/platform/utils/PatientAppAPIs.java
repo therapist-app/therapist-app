a20f508d5f12674302d032571d6ccca9b9861bda
Jonas Blum <jonas.blum2@uzh.ch>
2025-05-21 21:13:54 +0200

    .

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/PatientAppAPIs.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/PatientAppAPIs.java
@@ -1,7 +1,6 @@
 package ch.uzh.ifi.imrg.platform.utils;
 
 import ch.uzh.ifi.imrg.generated.api.PatientControllerPatientAPI;
-import ch.uzh.ifi.imrg.generated.invoker.ApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,8 +11,7 @@ public class PatientAppAPIs {
   public static PatientControllerPatientAPI patientControllerPatientAPI;
 
   @Autowired
-  public PatientAppAPIs(
-      @Value("${PATIENT_APP_URL}") String PATIENT_APP_URL) {
+  public PatientAppAPIs(@Value("${PATIENT_APP_URL}") String PATIENT_APP_URL) {
     System.out.println("PATIENT_APP_URL: " + PATIENT_APP_URL);
     patientControllerPatientAPI = new PatientControllerPatientAPI();
     patientControllerPatientAPI.getApiClient().setBasePath(PATIENT_APP_URL);
