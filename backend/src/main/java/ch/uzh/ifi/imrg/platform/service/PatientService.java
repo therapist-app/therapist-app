615b524a3e7680feb0c62a6a531fb62cbbcd2d59
Jonas Blum <jonas.blum2@uzh.ch>
2025-05-21 20:51:38 +0200

    .

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/service/PatientService.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/service/PatientService.java
@@ -31,7 +31,8 @@ public class PatientService {
 
   private final PatientMapper mapper = PatientMapper.INSTANCE;
 
-  @PersistenceContext private EntityManager entityManager;
+  @PersistenceContext
+  private EntityManager entityManager;
 
   public PatientService(
       @Qualifier("patientRepository") PatientRepository patientRepository,
@@ -42,10 +43,9 @@ public class PatientService {
   }
 
   public Patient registerPatient(String therapistId, CreatePatientDTO inputDTO) {
-    Therapist therapist =
-        therapistRepository
-            .findById(therapistId)
-            .orElseThrow(() -> new IllegalArgumentException("Therapist not found"));
+    Therapist therapist = therapistRepository
+        .findById(therapistId)
+        .orElseThrow(() -> new IllegalArgumentException("Therapist not found"));
 
     Patient patient = mapper.convertCreatePatientDtoToEntity(inputDTO);
     patient.setTherapist(therapist);
@@ -64,17 +64,18 @@ public class PatientService {
           .block();
     } catch (WebClientResponseException e) {
       logger.error(e.toString());
+    } catch (Error e2) {
+      logger.error(e2.toString());
     }
 
     return patient;
   }
 
   public Patient getPatientById(String patientId, Therapist loggedInTherapist) {
-    Patient foundPatient =
-        loggedInTherapist.getPatients().stream()
-            .filter(p -> p.getId().equals(patientId))
-            .findFirst()
-            .orElseThrow(() -> new EntityNotFoundException("Patient not found"));
+    Patient foundPatient = loggedInTherapist.getPatients().stream()
+        .filter(p -> p.getId().equals(patientId))
+        .findFirst()
+        .orElseThrow(() -> new EntityNotFoundException("Patient not found"));
     return foundPatient;
   }
 
