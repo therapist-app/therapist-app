901d2cd9560c321f80c3b0958eb9ddff16fe1f3d
Jonas Blum <jonas.blum2@uzh.ch>
2025-06-22 20:30:46 +0200

    Remvoving Overview pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,12 @@ import ChatBotTemplateEdit from './pages/chatBotTemplate/ChatBotTemplateEdit'
 import Dashboard from './pages/dashboard/Dashboard'
 import ExerciseCreate from './pages/exercises/ExerciseCreate.tsx'
 import ExerciseDetail from './pages/exercises/ExerciseDetail.tsx'
-import ExerciseOverview from './pages/exercises/ExerciseOverview.tsx'
 import Login from './pages/login/Login'
 import MeetingCreate from './pages/meetings/MeetingCreate.tsx'
 import MeetingDetail from './pages/meetings/MeetingDetail.tsx'
-import MeetingOverview from './pages/meetings/MeetingOverview.tsx'
 import NotFound from './pages/notFound/NotFound'
 import PatientCreate from './pages/patients/PatientCreate.tsx'
 import PatientDetail from './pages/patients/PatientDetail'
-import PatientsOverview from './pages/patients/PatientOverview'
 import Register from './pages/register/Register'
 import Settings from './pages/settings/settings.tsx'
 import GAD7test from './pages/tests/GAD7test'
@@ -45,13 +42,12 @@ const App = (): ReactElement => {
             <Route path={ROUTES[PAGES.REGISTRATION_PAGE]} element={<Register />} />
             <Route path={ROUTES[PAGES.LOGIN_PAGE]} element={<Login />} />
 
-            <Route path={ROUTES[PAGES.PATIENTS_OVERVIEW_PAGE]} element={<PatientsOverview />} />
             <Route path={ROUTES[PAGES.PATIENTS_CREATE_PAGE]} element={<PatientCreate />} />
             <Route path={ROUTES[PAGES.PATIENTS_DETAILS_PAGE]} element={<PatientDetail />} />
 
-            <Route path={ROUTES[PAGES.CHATBOT_OVERVIEW_PAGE]} element={<NotFound />} />
+            <Route path={ROUTES[PAGES.CHATBOT_CREATE_PAGE]} element={<NotFound />} />
+            <Route path={ROUTES[PAGES.CHATBOT_DETAILS_PAGE]} element={<NotFound />} />
 
-            <Route path={ROUTES[PAGES.MEETINGS_OVERVIEW_PAGE]} element={<MeetingOverview />} />
             <Route path={ROUTES[PAGES.MEETINGS_CREATE_PAGE]} element={<MeetingCreate />} />
             <Route path={ROUTES[PAGES.MEETINGS_DETAILS_PAGE]} element={<MeetingDetail />} />
 
@@ -59,9 +55,6 @@ const App = (): ReactElement => {
 
             <Route path={ROUTES[PAGES.EXERCISES_CREATE_PAGE]} element={<ExerciseCreate />} />
             <Route path={ROUTES[PAGES.EXERCISES_DETAILS_PAGE]} element={<ExerciseDetail />} />
-            <Route path={ROUTES[PAGES.EXERCISES_OVERVIEW_PAGE]} element={<ExerciseOverview />} />
-
-            <Route path={ROUTES[PAGES.CHATBOT_TEMPLATES_OVERVIEW_PAGE]} element={<NotFound />} />
 
             <Route
               path={ROUTES[PAGES.CHATBOT_TEMPLATES_DETAILS_PAGE]}
