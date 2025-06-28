e42f4f2c6c99fc214ff715db50a67c2eb13f55bd
Jonas Blum <jonas.blum2@uzh.ch>
2025-06-28 10:36:40 +0200

    cookie-security2 (#205)

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/EnvironmentVariables.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/EnvironmentVariables.java
@@ -20,18 +20,15 @@ public class EnvironmentVariables {
       @Value("${LOCAL_LLM_MODEL}") String LOCAL_LLM_MODEL,
       @Value("${LOCAL_LLM_URL}") String LOCAL_LLM_URL,
       @Value("${LOCAL_LLM_API_KEY}") String LOCAL_LLM_API_KEY,
-      @Value("${PATIENT_APP_URL}") String PATIENT_APP_URL) {
+      @Value("${PATIENT_APP_URL}") String PATIENT_APP_URL,
+      @Value("${APP_ENVIRONMENT:dev}") String APP_ENVIRONMENT) {
     EnvironmentVariables.JWT_SECRET_KEY = JWT_SECRET_KEY;
     EnvironmentVariables.LOCAL_LLM_MODEL = LOCAL_LLM_MODEL;
     EnvironmentVariables.LOCAL_LLM_URL = LOCAL_LLM_URL;
     EnvironmentVariables.LOCAL_LLM_API_KEY = LOCAL_LLM_API_KEY;
     EnvironmentVariables.PATIENT_APP_URL = PATIENT_APP_URL;
-    EnvironmentVariables.IS_PRODUCTION = isProductionEnvironment();
-  }
+    EnvironmentVariables.IS_PRODUCTION = APP_ENVIRONMENT.equalsIgnoreCase("prod");
 
-  private boolean isProductionEnvironment() {
-    String environment = System.getProperty("env", "dev");
-    System.out.println("environment: " + environment);
-    return "prod".equalsIgnoreCase(environment);
+    System.out.println("IS_PRODUCTION: " + EnvironmentVariables.IS_PRODUCTION);
   }
 }
