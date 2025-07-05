6958d984cdcd61df6fb84ee2e05d74ee0a06c41d
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-05 18:40:10 +0200

    .

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/service/MeetingService.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/service/MeetingService.java
@@ -27,8 +27,7 @@ public class MeetingService {
 
   private final MeetingRepository meetingRepository;
   private final PatientRepository patientRepository;
-  @PersistenceContext
-  private EntityManager entityManager;
+  @PersistenceContext private EntityManager entityManager;
 
   @Autowired
   public MeetingService(
@@ -48,11 +47,15 @@ public class MeetingService {
     meeting.setLocation(createMeetingDTO.getLocation());
     meeting.setPatient(patient);
     Meeting createdMeeting = meetingRepository.save(meeting);
-    CreateMeetingDTOPatientAPI createMeetingDTOPatientAPI = new CreateMeetingDTOPatientAPI()
-        .externalMeetingId(createdMeeting.getId()).startAt(meeting.getMeetingStart()).endAt(meeting.getMeetingEnd())
-        .location(meeting.getLocation());
+    CreateMeetingDTOPatientAPI createMeetingDTOPatientAPI =
+        new CreateMeetingDTOPatientAPI()
+            .externalMeetingId(createdMeeting.getId())
+            .startAt(meeting.getMeetingStart())
+            .endAt(meeting.getMeetingEnd())
+            .location(meeting.getLocation());
     PatientAppAPIs.coachMeetingControllerPatientAPI
-        .createMeeting1(createMeetingDTO.getPatientId(), createMeetingDTOPatientAPI).block();
+        .createMeeting1(createMeetingDTO.getPatientId(), createMeetingDTOPatientAPI)
+        .block();
     meetingRepository.flush();
     entityManager.refresh(createdMeeting);
 
@@ -76,6 +79,7 @@ public class MeetingService {
   public void deleteMeetingById(String meetingId) {
     Meeting meeting = meetingRepository.getReferenceById(meetingId);
     meeting.getPatient().getMeetings().remove(meeting);
-    PatientAppAPIs.coachMeetingControllerPatientAPI.deleteMeeting1(meeting.getPatient().getId(), meetingId);
+    PatientAppAPIs.coachMeetingControllerPatientAPI.deleteMeeting1(
+        meeting.getPatient().getId(), meetingId);
   }
 }
