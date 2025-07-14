81a327370b28f874340905305ac92ac19d63f1d4
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-14 23:07:05 +0200

    .

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/FileUtil.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/utils/FileUtil.java
@@ -1,18 +1,22 @@
 package ch.uzh.ifi.imrg.platform.utils;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUtil {
 
   public static File convertMultiPartFileToFile(MultipartFile file) throws IOException {
-    File convFile = new File(file.getOriginalFilename());
-    FileOutputStream fos;
-    fos = new FileOutputStream(convFile);
-    fos.write(file.getBytes());
-    fos.close();
-    return convFile;
+    File tempFile = Files.createTempFile("upload-", "-" + file.getOriginalFilename()).toFile();
+    try (InputStream is = file.getInputStream()) {
+      Files.copy(is, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+    tempFile.deleteOnExit();
+
+    return tempFile;
   }
 }
