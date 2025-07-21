501a28267f803edaa594a93f1baa97d0adabea97
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-21 16:11:42 +0200

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,12 @@
+// vite.config.ts
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    base: env.VITE_FRONTEND_PREFIX,
+  }
 })
