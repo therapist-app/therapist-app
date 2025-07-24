031efba8043467107dfc9c5e79fccb8ebd89b22a
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-24 18:42:35 +0200

    .

--- backend/src/main/java/ch/uzh/ifi/imrg/platform/service/CounselingPlanPhaseGoalService.java
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/service/CounselingPlanPhaseGoalService.java
@@ -1,20 +1,15 @@
 package ch.uzh.ifi.imrg.platform.service;
 
-import ch.uzh.ifi.imrg.platform.entity.CounselingPlan;
 import ch.uzh.ifi.imrg.platform.entity.CounselingPlanPhase;
 import ch.uzh.ifi.imrg.platform.entity.CounselingPlanPhaseGoal;
-import ch.uzh.ifi.imrg.platform.entity.Patient;
-import ch.uzh.ifi.imrg.platform.entity.Therapist;
 import ch.uzh.ifi.imrg.platform.repository.CounselingPlanPhaseGoalRepository;
 import ch.uzh.ifi.imrg.platform.repository.CounselingPlanPhaseRepository;
-import ch.uzh.ifi.imrg.platform.repository.TherapistRepository;
 import ch.uzh.ifi.imrg.platform.rest.dto.input.ChatMessageDTO;
 import ch.uzh.ifi.imrg.platform.rest.dto.input.CreateCounselingPlanPhaseGoalAIGeneratedDTO;
 import ch.uzh.ifi.imrg.platform.rest.dto.input.CreateCounselingPlanPhaseGoalDTO;
 import ch.uzh.ifi.imrg.platform.rest.dto.output.CounselingPlanPhaseGoalOutputDTO;
 import ch.uzh.ifi.imrg.platform.rest.mapper.CounselingPlanPhaseGoalMapper;
 import ch.uzh.ifi.imrg.platform.utils.ChatRole;
-import ch.uzh.ifi.imrg.platform.utils.LLMContextUtil;
 import ch.uzh.ifi.imrg.platform.utils.LLMUZH;
 import ch.uzh.ifi.imrg.platform.utils.SecurityUtil;
 import java.util.ArrayList;
@@ -29,17 +24,14 @@ public class CounselingPlanPhaseGoalService {
 
   private final CounselingPlanPhaseGoalRepository counselingPlanPhaseGoalRepository;
   private final CounselingPlanPhaseRepository counselingPlanPhaseRepository;
-  private final TherapistRepository therapistRepository;
 
   public CounselingPlanPhaseGoalService(
       @Qualifier("counselingPlanPhaseGoalRepository")
           CounselingPlanPhaseGoalRepository counselingPlanPhaseGoalRepository,
       @Qualifier("counselingPlanPhaseRepository")
-          CounselingPlanPhaseRepository counselingPlanPhaseRepository,
-      @Qualifier("therapistRepository") TherapistRepository therapistRepository) {
+          CounselingPlanPhaseRepository counselingPlanPhaseRepository) {
     this.counselingPlanPhaseGoalRepository = counselingPlanPhaseGoalRepository;
     this.counselingPlanPhaseRepository = counselingPlanPhaseRepository;
-    this.therapistRepository = therapistRepository;
   }
 
   public CounselingPlanPhaseGoalOutputDTO createCounselingPlanPhaseGoal(
@@ -63,8 +55,6 @@ public class CounselingPlanPhaseGoalService {
   public CreateCounselingPlanPhaseGoalDTO createCounselingPlanPhaseGoalAIGenerated(
       CreateCounselingPlanPhaseGoalAIGeneratedDTO dto, String therapistId) {
 
-    Therapist therapist = therapistRepository.getReferenceById(therapistId);
-
     CounselingPlanPhase phase =
         counselingPlanPhaseRepository
             .findById(dto.getCounselingPlanPhaseId())
@@ -75,14 +65,7 @@ public class CounselingPlanPhaseGoalService {
                             + dto.getCounselingPlanPhaseId()));
     SecurityUtil.checkOwnership(phase, therapistId);
 
-    CounselingPlan counselingPlan = phase.getCounselingPlan();
-
-    String systemPrompt = LLMContextUtil.getCounselingPlanContext(counselingPlan);
-
-    List<Patient> patientList = new ArrayList<>();
-    patientList.add(counselingPlan.getPatient());
-
-    systemPrompt += LLMContextUtil.getCoachAndClientContext(therapist, patientList);
+    String systemPrompt = phase.getCounselingPlan().getPatient().toLLMContext(0);
 
     String userPrompt =
         String.format(
