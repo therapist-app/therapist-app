3c802f44dad0f0af3cabf522c6d7abeaf3e37ab7
cedric-vr <115920587+cedric-vr@users.noreply.github.com>
2025-07-24 12:46:58 +0200

    #233: Display GAD7 tests from Client (#293)

---
+++ backend/src/main/java/ch/uzh/ifi/imrg/platform/rest/dto/output/PsychologicalTestQuestionOutputDTO.java
@@ -0,0 +1,12 @@
+package ch.uzh.ifi.imrg.platform.rest.dto.output;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PsychologicalTestQuestionOutputDTO {
+
+  private String question;
+  private int score;
+}
