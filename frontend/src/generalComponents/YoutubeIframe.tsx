0db07443fe57cc8f3040b09b2ea041276b8c668f
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-26 13:15:48 +0200

    .

--- frontend/src/generalComponents/YoutubeIframe.tsx
+++ frontend/src/generalComponents/YoutubeIframe.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 interface YoutubeIframeProps {
   youtubeUrl: string | undefined
 }
@@ -7,30 +8,43 @@ const getYoutubeEmbedUrl = (urlOrId: string | undefined): string | undefined => 
     return undefined
   }
 
+  let videoId: string | null = null
+
   const youtubeIdRegex =
     /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/
 
   const match = urlOrId.match(youtubeIdRegex)
 
   if (match && match[1]) {
-    const videoId = match[1]
-    return `https://www.youtube.com/embed/${videoId}`
+    videoId = match[1]
+  } else if (urlOrId.length === 11 && !urlOrId.includes(' ')) {
+    videoId = urlOrId
   }
 
-  if (urlOrId.length === 11 && !urlOrId.includes(' ')) {
-    return `https://www.youtube.com/embed/${urlOrId}`
+  if (videoId) {
+    const origin = window.location.origin
+    return `https://www.youtube.com/embed/${videoId}?origin=${origin}`
   }
 
   return undefined
 }
 
 const YoutubeIframe: React.FC<YoutubeIframeProps> = ({ youtubeUrl }) => {
+  const embedUrl = getYoutubeEmbedUrl(youtubeUrl)
+
+  if (!embedUrl) {
+    return <div>Invalid YouTube URL provided.</div>
+  }
+
   return (
     <iframe
       width='560'
       height='315'
-      src={getYoutubeEmbedUrl(youtubeUrl)}
+      src={embedUrl}
+      title='YouTube video player'
+      frameBorder='0'
       allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+      allowFullScreen
     ></iframe>
   )
 }
