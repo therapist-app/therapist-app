09445cd882b69d6dd1a4b2822791c628f497154e
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-26 13:15:58 +0200

    .

--- frontend/src/generalComponents/YoutubeIframe.tsx
+++ frontend/src/generalComponents/YoutubeIframe.tsx
@@ -42,7 +42,6 @@ const YoutubeIframe: React.FC<YoutubeIframeProps> = ({ youtubeUrl }) => {
       height='315'
       src={embedUrl}
       title='YouTube video player'
-      frameBorder='0'
       allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
       allowFullScreen
     ></iframe>
