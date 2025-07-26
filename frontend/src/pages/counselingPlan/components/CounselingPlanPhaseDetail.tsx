d1cd4d67ced18cf844f6d6b48bb41a6454d4a416
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-26 19:20:17 +0200

    .

--- frontend/src/pages/counselingPlan/components/CounselingPlanPhaseDetail.tsx
+++ frontend/src/pages/counselingPlan/components/CounselingPlanPhaseDetail.tsx
@@ -1,11 +1,16 @@
-import { IconButton, Typography } from '@mui/material'
-import { ReactElement } from 'react'
+import EditIcon from '@mui/icons-material/Edit'
+import { Button, IconButton, TextField, Typography } from '@mui/material'
+import { ReactElement, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { CounselingPlanPhaseOutputDTO } from '../../../api'
+import { CounselingPlanPhaseOutputDTO, UpdateCounselingPlanPhase } from '../../../api'
 import { useNotify } from '../../../hooks/useNotify'
 import DeleteIcon from '../../../icons/DeleteIcon'
-import { deleteCounselingPlanPhase } from '../../../store/counselingPlanSlice'
+import {
+  deleteCounselingPlanPhase,
+  updateCounselingPlanPhase,
+} from '../../../store/counselingPlanSlice'
+import { cancelButtonStyles, commonButtonStyles } from '../../../styles/buttonStyles'
 import { formatDateNicely } from '../../../utils/dateUtil'
 import { useAppDispatch } from '../../../utils/hooks'
 import AddCounselingPlanExercise from './AddCounselingPlanExercise'
@@ -31,8 +36,40 @@ const CounselingPlanPhaseDetail = ({
   const { notifyError, notifySuccess } = useNotify()
   const dispatch = useAppDispatch()
 
+  const [isEditing, setIsEditing] = useState(false)
+  const [updatePhaseName, setUpdatePhaseName] = useState(phase.phaseName)
+  const [updateDurationInWeeks, setUpdateDurationInWeeks] = useState(phase.durationInWeeks)
+
   const handleCreateCounselingPlanPhaseGoal = (): void => {
     onSuccess()
+  }
+
+  const handleClickEditPhase = (): void => {
+    setIsEditing(true)
+    setUpdatePhaseName(phase.phaseName)
+    setUpdateDurationInWeeks(phase.durationInWeeks)
+  }
+
+  const handleCancelUpdate = (): void => {
+    setIsEditing(false)
+    setUpdatePhaseName(phase.phaseName)
+    setUpdateDurationInWeeks(phase.durationInWeeks)
+  }
+
+  const handleSubmitUpdate = async (): Promise<void> => {
+    try {
+      const dto: UpdateCounselingPlanPhase = {
+        counselingPlanPhaseId: phase.id,
+        phaseName: updatePhaseName,
+        durationInWeeks: updateDurationInWeeks,
+      }
+      await dispatch(updateCounselingPlanPhase(dto)).unwrap()
+      setIsEditing(false)
+      notifySuccess(t('counseling_plan.phase_updated_success'))
+      onSuccess()
+    } catch (error) {
+      notifyError(typeof error === 'string' ? error : 'An unknown error occurred')
+    }
   }
 
   const handleDeletePhase = async (): Promise<void> => {
@@ -51,6 +88,10 @@ const CounselingPlanPhaseDetail = ({
         <Typography variant='h3'>
           {phaseNumber}. {phase.phaseName}
         </Typography>
+        <IconButton onClick={handleClickEditPhase}>
+          {' '}
+          <EditIcon sx={{ height: '25px', color: 'blue' }} />
+        </IconButton>
         {isLastPhase && (
           <IconButton onClick={handleDeletePhase} sx={{ width: 'fit-content' }}>
             <DeleteIcon />
@@ -59,16 +100,47 @@ const CounselingPlanPhaseDetail = ({
       </div>
 
       <div style={{ paddingLeft: '20px' }}>
-        <Typography>
-          {t('counseling_plan.start_date')}: {formatDateNicely(phase.startDate)}
-        </Typography>
-        <Typography>
-          {t('counseling_plan.end_date')}: {formatDateNicely(phase.endDate)}
-        </Typography>
-        <Typography>
-          {t('counseling_plan.duration')}: <strong>{phase.durationInWeeks}</strong>{' '}
-          {t('counseling_plan.weeks')}
-        </Typography>
+        {!isEditing ? (
+          <>
+            {' '}
+            <Typography>
+              {t('counseling_plan.start_date')}: {formatDateNicely(phase.startDate)}
+            </Typography>
+            <Typography>
+              {t('counseling_plan.end_date')}: {formatDateNicely(phase.endDate)}
+            </Typography>
+            <Typography>
+              {t('counseling_plan.duration')}: <strong>{phase.durationInWeeks}</strong>{' '}
+              {t('counseling_plan.weeks')}
+            </Typography>
+          </>
+        ) : (
+          <>
+            <TextField
+              required
+              fullWidth
+              label={t('counseling_plan.phase_name')}
+              value={updatePhaseName}
+              onChange={(e) => setUpdatePhaseName(e.target.value)}
+            />
+            <TextField
+              sx={{ marginTop: '20px' }}
+              label={t('counseling_plan.duration_in_weeks')}
+              value={updateDurationInWeeks}
+              type='number'
+              onChange={(e) => setUpdateDurationInWeeks(parseInt(e.target.value))}
+            />
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <Button sx={{ ...commonButtonStyles }} onClick={handleSubmitUpdate}>
+                {t('counseling_plan.update_phase')}
+              </Button>
+
+              <Button sx={{ ...cancelButtonStyles }} onClick={handleCancelUpdate}>
+                {t('counseling_plan.cancel')}
+              </Button>
+            </div>
+          </>
+        )}
 
         <Typography sx={{ marginTop: '20px', marginBottom: '10px' }} variant='h4'>
           {t('counseling_plan.goals')}:
